7be2e69ef0b1e93ae5b53419523b7af1e06739dc
pratikbadhe <43951072+pratikbadhe@users.noreply.github.com>
2018-10-21 23:04:26 +0530

    Add files via upload

---
+++ shopping.py.py
@@ -0,0 +1,65 @@
+from easygui import msgbox
+
+msgbox("Welcome to onlineshopee",title="comparekaro.com")
+b=buttonbox("Choose a category",title="Categories",choices="clothes","soap","chocolate","biscuits")
+if b=clothes:
+    t=buttonbox("choose brand",title="brands",choices="levis","ucb","Peterengland")
+        if t="levis":
+            m=buttonbox("choose seller",title="sellers",choices="siddhesh stores","karpe dealers","sanb clothes")
+               if a="siddhesh stores":
+                   msgbox(The price is inr1399)
+               elif a="karpe dealers":
+                   msgbox(The price is inr1019)
+               elif a="sanb clothes":
+                   msgbox(The price is inr1999)
+        if t="levis":
+            m=buttonbox("choose seller",title="sellers",choices="siddhesh stores","karpe dealers","sanb clothes")
+               if a="siddhesh stores":
+                   msgbox(The price is inr1099)
+               elif a="karpe dealers":
+                   msgbox(The price is inr1119)
+               elif a="sanb clothes":
+                   msgbox(The price is inr999)
+        if t="peterengland":
+            m=buttonbox("choose seller",title="sellers",choices="siddhesh stores","karpe dealers","sanb clothes")
+               if a="siddhesh stores":
+                   msgbox(The price is inr1899)
+               elif a="karpe dealers":
+                   msgbox(The price is inr1639)
+               elif a="sanb clothes":
+                   msgbox(The price is inr1999)           
+elif b=c:
+    t=buttonbox("choose brand",title="brands",choices="levis","ucb","Peterengland")
+        if t="levis":
+            m=buttonbox("choose seller",title="sellers",choices="siddhesh stores","karpe dealers","sanb clothes")
+               if a="siddhesh stores":
+                   msgbox(The price is inr1399)
+               elif a="karpe dealers":
+                   msgbox(The price is inr1019)
+               elif a="sanb clothes":
+                   msgbox(The price is inr1999)
+        if t="levis":
+            m=buttonbox("choose seller",title="sellers",choices="siddhesh stores","karpe dealers","sanb clothes")
+               if a="siddhesh stores":
+                   msgbox(The price is inr1099)
+               elif a="karpe dealers":
+                   msgbox(The price is inr1119)
+               elif a="sanb clothes":
+                   msgbox(The price is inr999)
+        if t="peterengland":
+            m=buttonbox("choose seller",title="sellers",choices="siddhesh stores","karpe dealers","sanb clothes")
+               if a="siddhesh stores":
+                   msgbox(The price is inr1899)
+               elif a="karpe dealers":
+                   msgbox(The price is inr1639)
+               elif a="sanb clothes":
+                   msgbox(The price is inr1999)           
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+install easygui
+
