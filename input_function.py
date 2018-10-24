d9f032c453180f2a965807b5068c564c4521d283
Pratik Badhe <43951072+pratikbadhe@users.noreply.github.com>
2018-10-24 23:22:12 +0530

    Add files via upload

---
+++ input_function.py
@@ -0,0 +1,8 @@
+name=input("kindly input your name=\t")
+grno=int(input("kindly enter your grno=\t"))
+rollno=int(input("kindly enter yoyr rollno=\t"))
+branch=input("kindly enter your branch=\t")
+division=input("kindly enter your division=\t")
+l=("\tname:",name,"\v\vtgrno:",grno,"\v\trollno:",rollno,"\v\tbranch:",branch,"\v\tdivision:",division)
+print(l[2:4])
+print(l)
