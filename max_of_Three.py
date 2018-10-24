50b0306cc432700db4dbc1dd632d4d82f1cfcd69
Pratik Badhe <43951072+pratikbadhe@users.noreply.github.com>
2018-10-24 22:35:59 +0530

    Add files via upload

---
+++ max_of_Three.py
@@ -0,0 +1,12 @@
+#Pratik Badhe
+#02
+#11810754
+def max_of_two(x,y):
+    if x>y:
+        return x
+    return y
+x=int(input())
+y=int(input())
+z=int(input())
+a=max_of_two(z,max_of_two(x,y))      
+print(a)
