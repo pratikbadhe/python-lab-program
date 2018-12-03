c13fa2ab6cdc3bba5b0b6e7df2d3aec05a9040ae
Pratik Badhe <43951072+pratikbadhe@users.noreply.github.com>
2018-12-03 19:15:19 +0530

    Add files via upload

---
+++ leap_year.py
@@ -0,0 +1,14 @@
+#Leap year
+#Pratik Badhe 02 11810754
+
+y=int(input("enter the year"))  
+if y%4==0:
+ if y%100==0:
+  if y%400==0:
+   print(" leap year")
+  else :
+    print("not a leap year")
+ else : 
+  print(" a leap year")
+else:
+ print("not a leap year")
