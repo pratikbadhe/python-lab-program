07fd990c98113743f7c35ae1f8b81688d583eaf0
Pratik Badhe <43951072+pratikbadhe@users.noreply.github.com>
2018-12-03 19:49:24 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,21 @@
+#LIST
+#Pratik Badhe
+#M-02 11810754
+name1=input("enter 1st student name")
+name2=input("enter 2nd student name")
+name3=input("enter 3rd student name")
+english1=input("marks of 1st student in english")
+english2=input("marks of 2nd student in english")
+english3=input("marks of 3rd student in english")
+science1=input("marks of 1st student in science")
+science2=input("marks of 2nd student in science")
+science3=input("marks of 3rd student in science")
+maths1=input("marks of 1st student in maths")
+maths2=input("marks of 2nd student in maths")
+maths3=input("marks of 3rd student in maths")
+student1=[name1,'english',english1,'science',science1,'maths',maths1]
+student2=[name2,'english',english2,'science',science2,'maths',maths2]
+student3=[name3,'english',english3,'science',science3,'maths',maths3]
+student=[student1,student2,student3]
+print(student)
+print(student[1])
